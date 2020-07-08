89b7007380a72928216b2269cce43097c1fc1a4c
MoisesRob <moisesrob@gmail.com>
2020-07-08 10:01:17 -0400

    build

---
+++ docs/precache-manifest.5a34f51d7bf275062b81c69f204f23a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "007f519ab4c07905db490836b5ce84a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2340b8aec335534f6230",
+    "url": "/static/css/main.d50af283.chunk.css"
+  },
+  {
+    "revision": "398d7c8b10c3c921877f",
+    "url": "/static/js/2.5f4a8c1b.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/static/js/2.5f4a8c1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2340b8aec335534f6230",
+    "url": "/static/js/main.9c7da86b.chunk.js"
+  },
+  {
+    "revision": "3d4175f9165103e13d56",
+    "url": "/static/js/runtime-main.b8be2bc2.js"
+  }
+]);
